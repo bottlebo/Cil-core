eec7058c5e9cd01d13cd996131d6583569f5afd4
trueshura <trueshura@mail.ru>
2020-09-14 14:20:43 +0600

    Fix receiptWorker

--- worker/receiptWorker.js
+++ worker/receiptWorker.js
@@ -14,8 +14,6 @@ module.exports = (DtoSerializer, Worker, Block, Transaction) => {
       data = data.filter(r => r);
       if (data.length) {
         for (let record of data) {
-          console.log('dumping receipt');
-          console.dir(record, {colors: true, depth: null});
           if (record.status) record.AddrTxSigner = await this._getContractSignerForTx(record.from);
         }
         await this._dumpObjectArray(data);
@@ -23,15 +21,13 @@ module.exports = (DtoSerializer, Worker, Block, Transaction) => {
     }
 
     async _getContractSignerForTx(txHash) {
-      console.log(`Searching for signer by "${txHash}"`);
-      const block = this.options.storage.findBlockByTxHash(txHash);
-      if(block) console.log(`Found block: "${block.getHash()}"`);
-      else {
-        console.log('Block NOT FOUND!');
+      const block = await this.options.storage.findBlockByTxHash(txHash);
+      if(block) {
+        const objFoundTx = block.txns.find(objTx => (new Transaction(objTx)).getHash() === txHash);
+        return objFoundTx ? (new Transaction(objFoundTx)).getTxSignerAddress() : undefined;
+      } else {
+        return undefined;
       }
-      const objFoundTx = block.txns.find(objTx => (new Transaction(objTx)).getTxHash() === txHash);
-      console.log(`Tx found: ${objFoundTx}`);
-      return objFoundTx ? new Transaction(objFoundTx) : undefined;
     }
   };
 };
