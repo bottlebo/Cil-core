ccde255b9e3747d41352b27cc2b0ae5681cea051
trueshura <trueshura@mail.ru>
2020-09-14 12:12:32 +0600

    Turn off API in favour of workers

--- worker/receiptWorker.js
+++ worker/receiptWorker.js
@@ -14,6 +14,8 @@ module.exports = (DtoSerializer, Worker, Block, Transaction) => {
       data = data.filter(r => r);
       if (data.length) {
         for (let record of data) {
+          console.log('dumping receipt');
+          console.dir(record, {colors: true, depth: null});
           if (record.status) record.AddrTxSigner = await this._getContractSignerForTx(record.from);
         }
         await this._dumpObjectArray(data);
@@ -21,8 +23,14 @@ module.exports = (DtoSerializer, Worker, Block, Transaction) => {
     }
 
     async _getContractSignerForTx(txHash) {
+      console.log(`Searching for signer by "${txHash}"`);
       const block = this.options.storage.findBlockByTxHash(txHash);
+      if(block) console.log(`Found block: "${block.getHash()}"`);
+      else {
+        console.log('Block NOT FOUND!');
+      }
       const objFoundTx = block.txns.find(objTx => (new Transaction(objTx)).getTxHash() === txHash);
+      console.log(`Tx found: ${objFoundTx}`);
       return objFoundTx ? new Transaction(objFoundTx) : undefined;
     }
   };
